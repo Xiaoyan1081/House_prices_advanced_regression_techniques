60e192975080482ca11cbf247b2dad89987a1576
elzawie:git config --global user.email zawieskal@gmail.comcatcat /c/Users/u6063854/Documents/Tools/Projectsgit config --global user.name elzawie <zawieskal@gmail.com>
2018-06-12 23:53:21 +0200

    'plot_suspicious' function finished, added docstrings

--- helper.py
+++ helper.py
@@ -1,27 +1,68 @@
 # Basic modules for dataframe manipulation
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy.stats import iqr
 from pandas.api.types import is_string_dtype, is_numeric_dtype, is_categorical_dtype
 
 
 
-# Converting objects(strings) into categories
+def lower_cols(dataframe):
+	"""
+	Function used to convert column headings to lower case
+	
+	Parameters:
+	
+	dataframe -just as parameter name implies, expects dataframe object
+	
+	"""
+	dataframe.columns = [x.lower() for x in dataframe.columns]
+
+
 def obj_to_cat(dataframe):
-    for n, c in dataframe.items():
-	    if is_string_dtype(c):
-		    dataframe[n] = c.astype('category').cat.as_ordered()
-    return dataframe
+   """
+	Function used to convert objects(strings) into categories
 	
+	Parameters:
 	
+	dataframe - just as parameter name implies, expects dataframe object
+	
+	"""
+
+   for n, c in dataframe.items():
+        if is_string_dtype(c):
+            dataframe[n] = c.astype('category').cat.as_ordered()
+   return dataframe
+	
+
+
 def fill_missing_nums(dataframe):    
+    """
+	 Function used to impute missing numerical values with column's median
+	
+	 Parameters:
+	
+	 dataframe -just as parameter name implies, expects dataframe object
+	
+   	 """
+    
     for n, c in dataframe.items(): 
         if is_numeric_dtype(c):
             if pd.isnull(c).sum() > 0:
                 dataframe.loc[:,n] = c.fillna(c.median())
     return dataframe
-	
+
+
 def fill_missing_cats(dataframe):
+    """
+    Function used to impute missing categorical values with column's mode
+	
+    Parameters:
+	
+    dataframe -just as parameter name implies, expects dataframe object
+	
+    """
     for n, c in dataframe.items():
         if is_categorical_dtype(c):
             if pd.isnull(c).sum() > 0:
@@ -29,20 +70,51 @@ def fill_missing_cats(dataframe):
     return dataframe
 	
 
+
 def display_cols(dataframe, type = 'category', num_samples = 7):
+	"""
+	Function used to display columns of desired data type
+	
+	Parameters:
+	
+	dataframe -just as parameter name implies, expects dataframe object
+	type - data type we are looking for
+	num_samples - number of rows to display
+	
+	"""
 	mask = dataframe.dtypes == type
 	return dataframe.loc[:, mask].sample(num_samples)
 	
 
+
 def display_nums_stats(dataframe):
+    """
+    Function used to calculate basic statistics of numerical columns.
+	
+    Parameters:
+	
+    dataframe -just as parameter name implies, expects dataframe object
+	
+    """
+
     numericals = []
     for n, c in dataframe.items():
 	    if is_numeric_dtype(c):
 		    numericals.append(n)
     return dataframe[numericals].describe()
 
+
 	
 def outliers_by_col(dataframe, multiplier = 1.5):
+	"""
+	Function used to determine outliers in each column.
+	
+	Parameters:
+	
+	dataframe -just as parameter name implies, expects dataframe object
+	multiplier - value used for calculating Tukey's Interquartile Range. By default we assume that all values lower than Q1 - (1.5 * IQR)  or greather than Q3 + (1.5 * IQR) are outliers
+	
+	"""
 	outliers_dict = {}
 	for column in dataframe.columns:
 			if is_numeric_dtype(dataframe[column]):
@@ -58,7 +130,42 @@ def outliers_by_col(dataframe, multiplier = 1.5):
 
 
 	
-def nominalnums_to_cat(dataframe, columns):
-    for column in columns:
-        dataframe[column] = dataframe[column].astype('category')
-    return dataframe
+def nominalnums_to_cat(dataframe, unique_values_split = 30,  return_boundary = 10):
+	"""
+	Function to convert nominal numerical features into categorical variables. 
+	
+	Parameters:
+	
+	dataframe -just as parameter name implies, expects dataframe object
+	unique_values_split - number of unique values to treat a variable as a categorical one. By default, variable's data type will be changed to 'category' if it has less than 30 unique values
+	return_boundary - decision boundary determining which variable names will be returned in list for further check. By default, all variables which take more than 10 unique values will be returned
+	
+	"""
+	cols_to_verify = []
+	for col in dataframe.columns:
+		if is_numeric_dtype(dataframe[col]):
+			length = len(dataframe[col].value_counts())
+			if ((length < unique_values_split) and ('area' not in col)):
+				dataframe[col] = dataframe[col].astype('category')
+				if (length > return_boundary):
+					cols_to_verify.append(col)
+	return(cols_to_verify)
+	
+
+def plot_suspicious(dataframe, columns):
+	"""
+	Function to plot suspicious categorical columns
+
+	Parameters:
+
+	dataframe - just as parameter name implies, expects dataframe object
+	columns - list of columns, e.g. those returned by 'nominalnums_to_cat' function
+
+	"""
+	length = len(columns)
+	plt.figure(figsize=(20,10))
+	for ix, col in enumerate(columns):
+		plt.subplot(np.ceil(length/3), 3, ix+1)
+		sns.countplot(dataframe[col], orient = 'v')
+
+		
